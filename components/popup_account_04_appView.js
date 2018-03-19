1c6837283873d20d86472d95790d7b50059cb4e4
STARSTREAM <starstream@STARSTREAMui-MacBook-Pro.local>
2018-03-19 14:52:02 +0900

    new file:   components/accountList.js 	modified:   components/popup_account_04_appView.js 	modified:   index.html

--- components/popup_account_04_appView.js
+++ components/popup_account_04_appView.js
@@ -1,70 +1,26 @@
-var html = require('choo/html')
-var path = require('path')
-
-var imgPath = path.join(__dirname,'../assets/images/ic_shoot_coin.svg');
+var html = require('choo/html');
+var accountList = require('./components/accountList');
 
 module.exports = function (state, emit) {
-return html`
-<body class="popup-dark">
-<p class="close"><a href="javascript:frameclose()" class="text-hide">창닫기</a> </p>
-<div class=" popup-bgimg">
-    <div class="container">
-        <header class="header clearfix">
-            <h3 class="popup-title">My Account</h3>
-            <h2 class="popup-logo text-hide">SHOOT</h2>
-        </header>
-
-            <div id="accountlist" class="account-list">
-
-                <div class="row list">
-                    <div class="col-md-8">
-                        <div class="row mb-3">
-                            <div class="col-md-3"><span class="badge badge-pill badge-style">ADDRESS</span></div>
-                            <div class="col-md-9 address">0x15fs8w2cqw3492582a</div>
+    return html`
+        <body class="popup-dark">
+            <p class="close"><a href="javascript:frameclose()" class="text-hide">창닫기</a> </p>
+            <div class=" popup-bgimg">
+                <div class="container">
+                    <header class="header clearfix">
+                        <h3 class="popup-title">My Account</h3>
+                        <h2 class="popup-logo text-hide">SHOOT</h2>
+                    </header>
+            
+                        <div id="accountlist" class="account-list">
+                            ${ accountList() }
                         </div>
-                        <div class="row">
-                            <div class="col-md-3"><span class="badge badge-pill badge-style">BALANCE</span></div>
-                            <div class="col-md-9 balance"><img src=${ imgPath } class="coin"> <span>1,056,850 <small>SHT</small></span></div>
-                        </div>
-                    </div>
-                    <div class="col-md-4"><a class="btn btn-lg btn-pointcolor col-sm-12" href=${ '/popup_usermode_appView' } role="button">입장 <small class="d-block">ENTRANCE</small></a></div>
+                        <!--//account-list-->
                 </div>
-
-                <div class="row list">
-                    <div class="col-md-8">
-                        <div class="row mb-3">
-                            <div class="col-md-3"><span class="badge badge-pill badge-style">ADDRESS</span></div>
-                            <div class="col-md-9 address">0x15fs8w2cqw3492582a</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3"><span class="badge badge-pill badge-style">BALANCE</span></div>
-                            <div class="col-md-9 balance"><img src=${ imgPath } class="coin"> <span>1,056,850 <small>SHT</small></span></div>
-                        </div>
-                    </div>
-                    <div class="col-md-4"><a class="btn btn-lg btn-pointcolor col-sm-12" href="popup_account_04_.html" role="button">입장 <small class="d-block">ENTRANCE</small></a></div>
-                </div>
-
-                <div class="row list">
-                    <div class="col-md-8">
-                        <div class="row mb-3">
-                            <div class="col-md-3"><span class="badge badge-pill badge-style">ADDRESS</span></div>
-                            <div class="col-md-9 address">0x15fs8w2cqw3492582a</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3"><span class="badge badge-pill badge-style">BALANCE</span></div>
-                            <div class="col-md-9 balance"><img src=${ imgPath } class="coin"> <span>1,056,850 <small>SHT</small></span></div>
-                        </div>
-                    </div>
-                    <div class="col-md-4"><a class="btn btn-lg btn-pointcolor col-sm-12" href="popup_account_04_.html" role="button">입장 <small class="d-block">ENTRANCE</small></a></div>
-                </div>
-
+                <!--//container-->
             </div>
-            <!--//account-list-->
-    </div>
-    <!--//container-->
-</div>
-<!--//popup-bgimg-->
-
-</body>
-`
+            <!--//popup-bgimg-->
+            
+        </body>
+    `
 }
